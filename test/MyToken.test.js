745551d2ecefb4c95294b75ec6a118182a0523a4
Dee Fisher <dekanifisher@yahoo.co.uk>
2021-08-12 15:02:47 +0100

    created default version of openzeppelin Crowdsale.sol contract

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -17,17 +17,28 @@ contract('Token Test', async (accounts) => {
     it('should have all tokens in my account', async () => {
         let instance = await Token.deployed();
         let totalSupply = await instance.totalSupply();
-        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
     });
 
     it('is possible to send tokens between accounts', async () => {
         const sendTokens = 1;
         let instance = await Token.deployed();
         let totalSupply = await instance.totalSupply();
-        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
-        //fulfilled property means promise gets fulfilled
-        expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
-        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(sendTokens));
-        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+        await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        //fulfilled property means promise gets fulfilled (as opposed to rejected)
+        await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+        await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(
+            totalSupply.sub(new BN(sendTokens)),
+        );
+        await expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+    });
+
+    it('is not possible to send more tokens than available in total', async () => {
+        let instance = await Token.deployed();
+        let balanceOfDeployer = await instance.balanceOf(deployerAccount);
+
+        await expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.eventually.be.rejected;
+
+        await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
     });
 });
