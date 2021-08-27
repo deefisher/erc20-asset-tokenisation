fd141eb61b50356f06aa29e4b6c8caf0e570cdc3
Dee Fisher <dekanifisher@yahoo.co.uk>
2021-08-27 12:50:36 +0100

    installed HDWalletProvider

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,29 @@
-const path = require("path");
+const path = require('path');
+const dotenv = require('dotenv');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Mnemonic = '';
+const AccountIndex = 0;
+
+console.log('mnemonic', process.env.MNEMONIC);
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-  networks: {
-    develop: {
-      port: 8545
-    }
-  },
-  compilers: {
-    solc: {
-      version: "0.8.0"
-    }
-  }
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    contracts_build_directory: path.join(__dirname, 'client/src/contracts'),
+    networks: {
+        develop: {
+            port: 8545,
+        },
+        ganache_local: {
+            provider: function () {
+                return new HDWalletProvider(Mnemonic, 'http://127.0.0.1:7545', AccountIndex);
+            },
+            network_id: 5777,
+        },
+    },
+    compilers: {
+        solc: {
+            version: '0.8.0',
+        },
+    },
 };
