338e71f8984e75c6d7bf6ea0c81e0dc227941df0
Dee Fisher <dekanifisher@yahoo.co.uk>
2021-09-09 16:40:47 +0100

    implemented infura

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,34 @@ module.exports = {
             host: '127.0.0.1',
             network_id: 5777,
         },
-        ganache_local: { //needed to create seperate wallet because metaMask creates its own account for the transaction but it won't be the owner account
+        ganache_local: {
+            //needed to create seperate wallet because metaMask creates its own account for the transaction but it won't be the owner account
             provider: function () {
                 return new HDWalletProvider(process.env.MNEMONIC, 'http://127.0.0.1:7545', AccountIndex);
             },
             network_id: 5777,
+        },
+        goerli_infura: {
+            provider: function () {
+                return new HDWalletProvider(
+                    process.env.MNEMONIC,
+                    'https://goerli.infura.io/v3/024d6723679345e5b82906a904336f6e',
+                    AccountIndex,
+                );
+            },
+            network_id: 5,
+        },
+        ropsten_infura: {
+            provider: function () {
+                return new HDWalletProvider(
+                    process.env.MNEMONIC,
+                    'https://ropsten.infura.io/v3/024d6723679345e5b82906a904336f6e',
+                    AccountIndex,
+                );
+            },
+            network_id: 3,
+            networkCheckTimeout: 1000000,
+            timeoutBlocks: 2000
         },
     },
     compilers: {
