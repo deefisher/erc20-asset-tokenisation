e8dd2701c79bf81a4bff61c11b81df71d3e7084e
Dee Fisher <dekanifisher@yahoo.co.uk>
2021-09-16 12:42:09 +0100

    implemented nft

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,11 @@
 const MyToken = artifacts.require('./MyToken.sol');
+const MyNFT = artifacts.require('./MyNFT.sol');
 require('dotenv').config({ path: '../.env' });
 
 module.exports = async function (deployer) {
+    let addr = await web3.eth.getAccounts();
+    console.log('addr[0]', addr[0]);
     await deployer.deploy(MyToken, process.env.INITIAL_TOKENS); //deploy the token contract
+    let nftInstance = await deployer.deploy(MyNFT); //deploy the token contract
+    await nftInstance.awardItem(addr[0], process.env.NFT_TOKEN_URI);
 };
