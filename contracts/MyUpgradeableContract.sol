c6977ca3a9fc2b0a90ea20738d9e636529fc2207
Dee Fisher <dekanifisher@yahoo.co.uk>
2021-09-28 11:55:40 +0100

    added getTokenAddress to MyUpgradeableContract.sol, refactored MyUpgradeableToken.test.js

--- contracts/MyUpgradeableContract.sol
+++ contracts/MyUpgradeableContract.sol
@@ -3,11 +3,17 @@ pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 
-contract MyUpgradeableContract is Initializable {
+contract MyUpgradeableContract is Initializable, OwnableUpgradeable {
     IERC20Upgradeable public token;
 
     function initialize(IERC20Upgradeable _token) public initializer {
+        __Ownable_init_unchained();
         token = _token;
+    }
+
+    function getTokenAddress() public view onlyOwner returns (address) {
+        return address(token);
     }
 }
--- test/MyUpgradeableToken.test.js
+++ test/MyUpgradeableToken.test.js
@@ -4,10 +4,14 @@ const tokenTestAssertions = require('./tokenTestAssertions');
 
 contract('UpgradeableToken Test', async (accounts) => {
     const [deployerAccount, recipient] = accounts;
+    let UpgradeableTokenInstance;
+
+    beforeEach(() => {
+        UpgradeableTokenInstance = UpgradeableToken.deployed();
+    });
 
     //eventually property is needed to allow for promises to resolve
     it('should have all tokens in my account', async () => {
-        const UpgradeableTokenInstance = UpgradeableToken.deployed();
         await tokenTestAssertions.assertAllTokensInDeployerAccount({
             token: UpgradeableTokenInstance,
             deployerAccount,
@@ -16,7 +20,6 @@ contract('UpgradeableToken Test', async (accounts) => {
     });
 
     it('is possible to send tokens between accounts', async () => {
-        const UpgradeableTokenInstance = UpgradeableToken.deployed();
         await tokenTestAssertions.assertSendTokensBetweenAccounts({
             token: UpgradeableTokenInstance,
             deployerAccount,
@@ -25,7 +28,6 @@ contract('UpgradeableToken Test', async (accounts) => {
     });
 
     it('is not possible to send more tokens than available in total', async () => {
-        const UpgradeableTokenInstance = UpgradeableToken.deployed();
         await tokenTestAssertions.assertNotPossibleToSendMoreTokensThanAvailable({
             token: UpgradeableTokenInstance,
             deployerAccount,
